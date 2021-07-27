66195b9e230bad806208aaa6b0c1a132651868fd
Marco Costanzo <marco.costanzo@unicampania.it>
2021-07-27 11:10:06 +0200

    add print on error

--- include/sun_ros_yaml_msgs/yaml_io.h
+++ include/sun_ros_yaml_msgs/yaml_io.h
@@ -1,46 +1,44 @@
 #ifndef SUN_ROS_YAML_MSGS_YAML_IO_H
 #define SUN_ROS_YAML_MSGS_YAML_IO_H
 
-#include <fstream>
 #include "yaml-cpp/yaml.h"
+#include <fstream>
 
-namespace YAML
-{
+namespace YAML {
 
-    template <typename T>
-    void saveOnFile(const std::string &file, T resource, const std::string &root_yaml_node = "")
-    {
-        YAML::Node out_yaml;
-        if (root_yaml_node == "" || root_yaml_node.empty())
-        {
-            out_yaml = resource;
-        }
-        else
-        {
-            out_yaml[root_yaml_node] = resource;
-        }
+template <typename T>
+void saveOnFile(const std::string &file, T resource,
+                const std::string &root_yaml_node = "") {
+  YAML::Node out_yaml;
+  if (root_yaml_node == "" || root_yaml_node.empty()) {
+    out_yaml = resource;
+  } else {
+    out_yaml[root_yaml_node] = resource;
+  }
 
-        YAML::Emitter yaml_emitter;
-        yaml_emitter << out_yaml;
-        std::ofstream outfile(file);
-        outfile << yaml_emitter.c_str();
-        outfile.close();
-    }
-
-    template <typename T>
-    T loadFromFile(const std::string &file, const std::string &root_yaml_node = "")
-    {
-        YAML::Node db_yaml = YAML::LoadFile(file);
-        if (root_yaml_node == "" || root_yaml_node.empty())
-        {
-            return db_yaml.as<T>();
-        }
-        else
-        {
-            return db_yaml[root_yaml_node].as<T>();
-        }
-    }
-
+  YAML::Emitter yaml_emitter;
+  yaml_emitter << out_yaml;
+  std::ofstream outfile(file);
+  outfile << yaml_emitter.c_str();
+  outfile.close();
 }
+
+template <typename T>
+T loadFromFile(const std::string &file,
+               const std::string &root_yaml_node = "") {
+  try {
+    YAML::Node db_yaml = YAML::LoadFile(file);
+    if (root_yaml_node == "" || root_yaml_node.empty()) {
+      return db_yaml.as<T>();
+    } else {
+      return db_yaml[root_yaml_node].as<T>();
+    }
+  } catch (const YAML::Exception &e) { 
+      std::cerr << "YAML::loadFromFile error [" + file + "](/" + root_yaml_node + ")\n";
+      throw;
+  }
+}
+
+} // namespace YAML
 
 #endif
